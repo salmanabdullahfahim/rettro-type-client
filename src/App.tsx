9372a8c3da6d9b6a4cea17e807f06628ea1bac06
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-07-08 20:47:52 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-400">Helloo Rettro Type</h1>
+      <Button>Login</Button>
+    </>
+  );
+}
+
+export default App;
