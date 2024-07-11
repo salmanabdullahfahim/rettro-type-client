f68fac96d495281799b9976d8211d7a738a81383
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-07-11 12:20:03 +0600

    cartSlice added in features

---
+++ src/redux/features/cartSlice.ts
@@ -0,0 +1,126 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
+
+export interface Product {
+  _id: string;
+  image: string;
+  title: string;
+  brand: string;
+  availableQuantity: number;
+  price: number;
+  rating: number;
+  description: string;
+}
+
+// Cart item interface
+export interface CartItem extends Product {
+  cartQuantity: number;
+}
+
+// Initial state interface
+export interface CartState {
+  items: CartItem[];
+  totalOrderPrice: number;
+}
+
+const initialState: CartState = {
+  items: [],
+  totalOrderPrice: 0,
+};
+
+// Utility function to show sweet alert
+const showAlert = (message: string) => {
+  Swal.fire({
+    title: message,
+    text: "Out of Stock!",
+    icon: "error",
+  });
+};
+
+// Cart slice
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Product>) => {
+      console.log(action.payload);
+      const product = action.payload;
+      const existingItem = state.items.find((item) => item._id === product._id);
+
+      console.log(existingItem);
+
+      if (existingItem) {
+        console.log("exist");
+        if (existingItem.availableQuantity > 0) {
+          existingItem.availableQuantity = existingItem.availableQuantity - 1;
+          existingItem.cartQuantity += 1;
+          state.totalOrderPrice += product.price;
+        } else {
+          console.log("exist");
+          return showAlert("Product is out of stock!");
+        }
+      } else {
+        console.log("new");
+
+        if (product.availableQuantity > 0) {
+          product.availableQuantity = product.availableQuantity - 1;
+          state.items.push({ ...product, cartQuantity: 1 });
+          state.totalOrderPrice += product.price;
+        } else {
+          showAlert("Product is out of stock!");
+        }
+      }
+    },
+
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      const productId = action.payload;
+      const existingItem = state.items.find((item) => item._id === productId);
+
+      if (existingItem) {
+        state.totalOrderPrice -= existingItem.price * existingItem.cartQuantity;
+        existingItem.availableQuantity += existingItem.cartQuantity;
+        state.items = state.items.filter((item) => item._id !== productId);
+      }
+    },
+    incrementQuantity: (state, action: PayloadAction<string>) => {
+      const productId = action.payload;
+      const existingItem = state.items.find((item) => item._id === productId);
+
+      if (existingItem) {
+        if (existingItem.availableQuantity > 0) {
+          existingItem.cartQuantity += 1;
+          existingItem.availableQuantity -= 1;
+          state.totalOrderPrice += existingItem.price;
+        } else {
+          showAlert("Product is out of stock!");
+        }
+      }
+    },
+    decrementQuantity: (state, action: PayloadAction<string>) => {
+      const productId = action.payload;
+      const existingItem = state.items.find((item) => item._id === productId);
+
+      if (existingItem) {
+        if (existingItem.cartQuantity > 1) {
+          existingItem.cartQuantity -= 1;
+          existingItem.availableQuantity += 1;
+          state.totalOrderPrice -= existingItem.price;
+        } else {
+          state.totalOrderPrice -= existingItem.price;
+          existingItem.availableQuantity += 1;
+          state.items = state.items.filter((item) => item._id !== productId);
+        }
+      }
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
