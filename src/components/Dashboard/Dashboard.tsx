1d5d37b99366e3849d06107ba384225f03d0fcf5
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-07-13 13:41:26 +0600

    dashboard text fix

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-center text-2xl mx-auto font-semibold">
-      Welcome to Rettro Type Dashboard
+    <div className="flex items-center justify-center text-3xl mx-auto font-semibold">
+      Welcome to Rettro Type's Product Management Dashboard
     </div>
   );
 };
